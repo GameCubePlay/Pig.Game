7ee0f15ba6991f770b2c5671ce03afdcc2528440
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-24 16:55:42 +0200

    Switching the active player

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0')
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
 const currentEl = document.getElementById('current--1')
@@ -13,7 +14,9 @@ score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
 
+const score = [0.0]
 let currentScore = 0
+let activePlayer = 0
 
 btnRoll.addEventListener('click', function(){
     const dice = Math.trunc(Math.random() * 6) + 1
@@ -22,8 +25,11 @@ btnRoll.addEventListener('click', function(){
 
     if(dice !== 1){
         currentScore += dice
-        currentEl.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
     } else {
-
+        activePlayer = activePlayer === 0 ? 1 : 0 
+        currentScore = 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
     }
 })
